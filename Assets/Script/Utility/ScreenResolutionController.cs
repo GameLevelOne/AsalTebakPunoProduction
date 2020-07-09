632db5d970dcb1fd727b7de35006d389de1dd2c2
anasrasyid <anasrasyid.gamelevelone@gmail.com>
2020-07-09 18:12:30 +0700

    Fixing anchor UI.
    details:
    - add script to scale resolution.

---
+++ Assets/Script/Utility/ScreenResolutionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenResolutionController : MonoBehaviour {
+
+	void Start () {
+		if(2 * Screen.width <= Screen.height)
+        {
+            var canvasScaler = this.GetComponent<CanvasScaler>();
+            if (canvasScaler)
+            {
+                if (canvasScaler.screenMatchMode == CanvasScaler.ScreenMatchMode.MatchWidthOrHeight)
+                {
+                    canvasScaler.matchWidthOrHeight = 0;
+                }
+            }
+        }
+	}
+
+}
